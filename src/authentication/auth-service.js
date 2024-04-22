02d0e6792b1fcebf8b8543242d1c1f16f514f054
pfernandez <pedrofernbel@gmail.com>
2024-04-22 18:58:50 +0200

    feat: added password hashing to register.

--- src/authentication/auth-service.js
+++ src/authentication/auth-service.js
@@ -1,3 +1,6 @@
+import hashPassword from "../utils/treatment-utils.js/hashPassword.js";
+import isValidEmail from "../utils/validators/isValidEmail.js";
+import isValidPassword from "../utils/validators/isValidPassword.js";
 import { registerUser } from "./auth-repository.js";
 
 export const registerService = async (data) => {
@@ -9,10 +12,18 @@ export const registerService = async (data) => {
         }
 
         //TODO verifications
+        if (!isValidEmail(email)) {
+            throw new Error('Invalid email');
+        }
+
+        if (!isValidPassword(password)) {
+            throw new Error('Invalid password');
+        }
 
         //TODO hash password
+        const hashedPassword = await hashPassword(password);
 
-        registerUser(name, email, password);
+        registerUser(name, email, hashedPassword);
     } catch (error) {
         throw error;
     }
