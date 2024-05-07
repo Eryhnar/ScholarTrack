49371854e228d4d904dcea2935c8e3e3be309c6d
pfernandez <pedrofernbel@gmail.com>
2024-05-07 16:22:20 +0200

    feat: created task controller.

---
+++ src/entities/Task/task-controller.js
@@ -0,0 +1,18 @@
+
+export const createTask = async (req, res) => {
+    try {
+        const userId = req.tokenData.userId;
+        const { groupId } = req.params;
+        const taskInfo = req.body;
+        const task = await createTaskService(userId, groupId, taskInfo);
+        res.status(201).json(
+            {
+                success: true,
+                message: "Task created successfully",
+                data: task,
+            }
+        );
+    } catch (error) {
+        next(error);
+    }
+}
