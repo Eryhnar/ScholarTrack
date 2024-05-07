ad0bbcd2c8171b79ab5951af3bdc2793bafa1390
pfernandez <pedrofernbel@gmail.com>
2024-05-07 16:28:28 +0200

    feat: created task service.

---
+++ src/entities/Task/task-service.js
@@ -0,0 +1,27 @@
+
+export const createTaskService = async (userId, groupId, taskInfo) => {
+    try {
+        const { name, description, deadline, weight, optional, tags } = taskInfo;
+        const newTask = {}
+
+        if (!name) {
+            throw new InvalidInputError(400, "Name is required");
+        }
+        newTask.name = name;
+
+        if (!weight) {
+            throw new InvalidInputError(400, "Weight is required");
+        }
+        newTask.weight = weight;
+
+        if (description) newTask.description = description;
+        if (deadline) newTask.deadline = deadline;
+        if (optional) newTask.optional = optional;
+        if (tags) newTask.tags = tags;
+
+        const task = await createTaskRepository(newTask);
+        return task;
+    } catch (error) {
+        throw error;
+    }
+}
