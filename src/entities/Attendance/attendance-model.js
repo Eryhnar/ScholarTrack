3696877954861eab455f10f78ec01abbfb020942
pfernandez <pedrofernbel@gmail.com>
2024-05-09 11:27:06 +0200

    feat: created attendance model.

---
+++ src/entities/Attendance/attendance-model.js
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+
+const attendanceSchema = new Schema(
+    {
+        date: {
+            type: Date,
+            required: true,
+        },
+        student: {
+            type: Schema.Types.ObjectId,
+            ref: "Student",
+            required: true,
+        },
+        group: {
+            type: Schema.Types.ObjectId,
+            ref: "Group",
+            required: true,
+        },
+        present: {
+            type: Boolean,
+            required: true,
+            default: false, //might not be needed
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+)
+
+attendanceSchema.index({ student: 1, date: 1, group: 1 }, { unique: true });
+
+const Attendance = model("Attendance", attendanceSchema);
+
+export default Attendance;
